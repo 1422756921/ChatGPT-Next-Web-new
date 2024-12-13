3f925f41dd5b1d1f1582ef0d329f2eccc604655a
Qingyun Yixi <1422756921@qq.com>
2024-12-13 11:46:19 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "ChatGPT-青云依兮",
+  description: "您的个人 ChatGPT 聊天机器人",
   appleWebApp: {
-    title: "NextChat",
+    title: "ChatGPT-青云依兮",
     statusBarStyle: "default",
   },
 };
