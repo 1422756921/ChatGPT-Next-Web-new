fd762260fd16efac25b679a3388ec0dc35e731cc
Qingyun Yixi <1422756921@qq.com>
2024-12-13 11:44:27 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,5 +1,7 @@
 export const OWNER = "ChatGPTNextWeb";
 export const REPO = "ChatGPT-Next-Web";
+export const BLOG_URL = `https://blog.aerr.cn`;
+export const FAKA_URL = `http://fk.ikun.bf`;
 export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
 export const PLUGINS_REPO_URL = `https://github.com/${OWNER}/NextChat-Awesome-Plugins`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
